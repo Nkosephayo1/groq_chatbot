d1f4f5f789837097a39a46853958094720dee73e
Nkosephayo1 <zphayo@gmail.com>
2026-09-22 00:44:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()  # Load environment variables from .env file
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 # Default model
-GROQ_MODEL = "llama-3.1-8b-instant"
+GROQ_MODEL = "llama-3.1-70b-versatile"
 
 if GROQ_API_KEY is None:
     print("⚠️ WARNING: GROQ_API_KEY is not set!")
